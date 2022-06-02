144ccbe0c07eda11bcbf04694290d7d280012a54
Bhashitha Arambepola <barambepola@gmail.com>
2022-06-02 18:01:55 +0530

    change ts file

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,8 @@
 const pointer=document.getElementById('pointer')!;
 
 window.addEventListener('mousemove',(e)=>{
+    console.log("Working");
     pointer.style.left= (e.clientX+15)+'px';
     pointer.style.top= (e.clientY+15)+'px';
+
 });
